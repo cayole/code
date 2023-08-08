9db285c7d7718f61bdf9f455d87d5de456bd24e9
Luozhen Yue <158067162@qq.com>
2023-08-08 20:23:08 +0800

    stack

--- C/stack/src/main.c
+++ C/stack/src/main.c
@@ -2,20 +2,34 @@
 
 int main()
 {
-	Queue* queue = InitQueue(8);
+	Stack* stack = InitStack(10);
+	Queue* queue = InitQueue(10);
 
-	EnQueue(queue, 10);
-	EnQueue(queue, 9);
-	EnQueue(queue, 8);
-	EnQueue(queue, 7);
+	for (int i = 0; i < 10; i++) {
+		EnQueue(queue, i);
+		Push(stack, i);
+	}
+
+	printf("IfStackEmpty=%d\n", IfStackEmpty(stack));
+	printf("IfQueueEmpty=%d\n", IfQueueEmpty(queue));
+	printf("IfStackFull=%d\n", IfStackFull(stack));
+	printf("IfQueueFull=%d\n", IfQueueFull(queue));
 
 	float rec;
-	DeQueue(queue, &rec);
-	printf("%.2f\n", rec);
-	DeQueue(queue, &rec);
-	printf("%.2f\n", rec);
-	DeQueue(queue, &rec);
-	printf("%.2f", rec);
+	for (int i = 0; i < 4; i++) {
+		Pop(stack, &rec);
+		printf("%.2f\n", rec);
+		DeQueue(queue, &rec);
+		printf("%.2f\n", rec);
+	}
+
+	ClearStack(stack);
+	ClearQueue(queue);
+	printf("IfStackEmpty=%d\n", IfStackEmpty(stack));
+	printf("IfQueueEmpty=%d\n", IfQueueEmpty(queue));
+
+	DeleteStack(stack);
+	DeleteQueue(queue);
 
 	return 0;
 }
